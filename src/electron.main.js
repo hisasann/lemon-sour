ae497a6d418827c6722bec42fb7608de0febab1f
hisasann <hisasann25@gmail.com>
2017-02-16 18:53:40 +0900

    add lemon-sour-man using electron

---
+++ src/electron.main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 256,
+    height: 332,
+    frame: false
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+ipcMain.on('close', (event, arg) => {
+  app.quit();
+});
